866ff15cff0c9dc4c8cc0b6d7a120d38118a486d
chungmcl <chungmcl@hotmail.com>
2021-11-27 01:24:48 -0800

    init

---
+++ clean.py
@@ -0,0 +1,33 @@
+import re
+
+class Hanzi:
+    def __init__(self, char, jyutpings):
+            self.char = char
+            self.jyutpings = jyutpings
+
+    def __lt__(self, other):
+        return self.char < other.char
+
+lines = []
+with open('charlist.csv', "r") as file:
+    lines = file.readlines()
+
+hanzis = []
+for line in lines:
+    line_split = line.split(',', 1)
+    char = line_split[0]
+    jyutpings = re.findall('""([^"]*)""', line_split[1])
+    hanzis.append(Hanzi(char, jyutpings))
+hanzis.sort(key=lambda x: x.char, reverse=False)
+
+out = "const int PRONUNCIATIONS_LENGTH = " + str(len(hanzis)) + ";\n"
+out += "char* pronunciations[][2] =\n{\n"
+for hanzi in hanzis:
+    out += "\t{ "
+    out += "\"" + hanzi.char + "\"" + ", "
+    out += "\"" + ":".join(hanzi.jyutpings) + "\""
+    out += " },\n"
+out += "};"
+
+with open('jyutping.h', "w") as file:
+    file.write(out)
---
+++ jyutping.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "jyutping.h"
+
+int find_entry(int l, int r, char* x)
+{
+    if (r >= l) {
+        int mid = l + (r - l) / 2;
+        if (strcmp(pronunciations[mid][0], x) == 0)
+            return mid;
+        if (strcmp(pronunciations[mid][0], x) > 0)
+            return find_entry(l, mid - 1, x);
+        return find_entry(mid + 1, r, x);
+    }
+    return -1;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc > 1) {
+        // print out jyutping
+        int input_length = strlen(argv[1]);
+        for (int i = 0; i < input_length; i += 3) {
+            char buff[4]; // unicode chinese chars are 3 bytes + \0 = 4 bytes
+            memcpy(buff, argv[1] + i, 3);
+            buff[3] = '\0';
+            printf("%s  %s\n", buff, pronunciations[find_entry(0, PRONUNCIATIONS_LENGTH, buff)][1]);
+        }
+        printf("\n");
+
+        // speak 
+        char speech_command[strlen("say -v sin-ji ") + strlen(argv[1])];
+        strcpy(speech_command, "say -v sin-ji ");
+        strcat(speech_command, argv[1]);
+        system(speech_command);
+    }
+}
+
