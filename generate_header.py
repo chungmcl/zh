a5a1a0fa85ee2a8d38b0be98759f06a9cf0795a4
chungmcl <chungmcl@hotmail.com>
2021-11-27 19:01:05 -0800

    made jyutping show up in order of magnitude of commonality

--- generate_header.py
+++ generate_header.py
@@ -7,7 +7,9 @@ from hanziconv import HanziConv
 class Hanzi:
     def __init__(self, char, jyutpings):
             self.char = char
+            jyutpings = [(jyutping[0], int(jyutping[1])) for jyutping in jyutpings] # convert 'commanality' score to int so it can be sorted
             self.jyutpings = jyutpings
+            self.jyutpings.sort(key=lambda x: x[1], reverse=True) # sort by 'commonality' score
 
     def __lt__(self, other):
         return self.char < other.char
@@ -20,21 +22,21 @@ hanzis = []
 for i in range(1, len(lines)):
     line_split = lines[i].split(',', 1)
     char = line_split[0]
-    jyutpings = re.findall('""([^"]*)""', line_split[1])
+    jyutpings = re.findall('""([^"]*)"":\s+(\d+)', line_split[1])
     hanzis.append(Hanzi(char, jyutpings))
     charSimplified = HanziConv.toSimplified(char)
-    if (charSimplified != char):
+    if (charSimplified != char): # if simplified variant exists, add it too
         hanzis.append(Hanzi(charSimplified, jyutpings))
-hanzis.sort(key=lambda x: x.char, reverse=False)
+hanzis.sort(key=lambda x: x.char, reverse=False) # sort by unicode
 
 out = "const int PRONUNCIATIONS_LENGTH = " + str(len(hanzis)) + ";\n"
 out += "char* PRONUNCIATIONS[][2] =\n{\n"
 for hanzi in hanzis:
     out += "\t{ "
     out += "\"" + hanzi.char + "\"" + ", "
-    out += "\"" + ":".join(hanzi.jyutpings) + "\""
+    out += "\"" + ":".join([jyutping[0] for jyutping in hanzi.jyutpings]) + "\""
     out += " },\n"
-out = out[0:len(out) - 2] + "\n" # remove extra , and replace newline
+out = out[0:len(out) - 2] + "\n" # remove extra ',' and replace newline
 out += "};"
 
 with open('jyutping.h', "w") as file:
