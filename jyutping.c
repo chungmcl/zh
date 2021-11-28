56c277939e83024b187efb81456a48700b40aa79
chungmcl <chungmcl@hotmail.com>
2021-11-27 17:54:34 -0800

    fixed issue with mixed encoding inputs

--- jyutping.c
+++ jyutping.c
@@ -19,20 +19,42 @@ int find_entry(int l, int r, char* x)
 
 int main(int argc, char* argv[]) {
     if (argc > 1) {
-        // print out jyutping
-        int input_length = strlen(argv[1]);
-        for (int i = 0; i < input_length; i += 3) {
-            char buff[4]; // unicode chinese chars are 3 bytes + \0 = 4 bytes
-            memcpy(buff, argv[1] + i, 3);
-            buff[3] = '\0';
-            printf("%s\t%s\n", buff, PRONUNCIATIONS[find_entry(0, PRONUNCIATIONS_LENGTH, buff)][1]);
+        int input_byte_count = strlen(argv[1]); // strlen returns ASCII strlen, but we may have unicode
+        unsigned char* byte = (unsigned char*)argv[1];
+        while (*byte != '\0') {
+            // Thanks to @paul.ago on StackOverflow 
+            // Binary    Hex          Comments
+            // 0xxxxxxx  0x00..0x7F   Only byte of a 1-byte character encoding
+            // 10xxxxxx  0x80..0xBF   Continuation byte: one of 1-3 bytes following the first
+            // 110xxxxx  0xC0..0xDF   First byte of a 2-byte character encoding
+            // 1110xxxx  0xE0..0xEF   First byte of a 3-byte character encoding
+            // 11110xxx  0xF0..0xF7   First byte of a 4-byte character encoding
+            int token_length = 1;
+            if (*byte >= 0xC0 && *byte <= 0xDF) {
+                token_length = 2;
+            }
+            else if (*byte >= 0xE0 && *byte <= 0xEF) {
+                token_length = 3;
+            }
+            else if (*byte >= 0xF0) {
+                token_length = 4;
+            }
+
+            char buff[5]; 
+            memcpy(buff, byte, token_length); 
+            buff[token_length] = '\0';
+            if (token_length >= 3) printf("%s\t\t%s\n", buff, PRONUNCIATIONS[find_entry(0, PRONUNCIATIONS_LENGTH, buff)][1]);
+            else printf("%s\n", buff);
+
+            byte += token_length;
         }
 
-        #if __APPLE__
-        // speak
-        char speech_command[strlen("say -v sin-ji ") + strlen(argv[1])];
-        strcpy(speech_command, "say -r 87 -v sin-ji ");
+        #if __APPLE__ // If on macOS, call say command to speak
+        // Equivalent expression: strlen("say -v sin-ji ") + strlen("\"") + strlen(argv[1]) + strlen("\"");
+        char speech_command[strlen("say -v sin-ji ") + strlen(argv[1]) + 4];
+        strcpy(speech_command, "say -r 87 -v sin-ji \"");
         strcat(speech_command, argv[1]);
+        strcat(speech_command, "\"");
         system(speech_command);
         #endif
     }
