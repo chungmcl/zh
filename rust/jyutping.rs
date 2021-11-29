95d787ec8207e6f392927cf01f48d5e2c915b722
chungmcl <chungmcl@hotmail.com>
2021-11-28 20:29:55 -0800

    Got rust version working

--- rust/jyutping.rs
+++ rust/jyutping.rs
@@ -1,49 +1,46 @@
-mod constants;
-
+#![feature(char_indices_offset)]
 use std::env;
 use std::process::Command;
 use std::cmp::Ordering;
+use std::str::CharIndices;
+
+mod constants;
 use constants::DICT_LENGTH;
 use constants::DICT;
 
 fn find_entry(l: usize, r: usize, x: &str) -> usize {
     if r >= l {
         let mid: usize = l + (r - l) / 2;
-        println!("{} VS {}: {:?}", DICT[mid][0], x, DICT[mid][0].cmp(x));
-        if DICT[mid][0].cmp(x) == Ordering::Equal {
-            //println!("equal");
+        if DICT[mid][0].eq(x) {
             return mid;
         }
         if DICT[mid][0].cmp(x) == Ordering::Greater {
-            //println!("greater");
             return find_entry(l, mid - 1, x);
         }
-        //println!("less");
         return find_entry(mid + 1, r, x);
     }
     return usize::MAX;
 }
 
 fn main() {
-    let a = "起";
-    let b = "起";
-    println!("{:?}", a.cmp(b));
-
     let args: Vec<String> = env::args().collect();
-
     if args.len() > 1 {
-        for c in args[1].chars() {
-            let mut tmp: [u8; 4] = [0; 4];
-            c.encode_utf8(&mut tmp);
-            let loc: usize = find_entry(0, DICT_LENGTH, &String::from_utf8_lossy(&tmp));
-            println!("{}", loc);
+        let mut char_itr: CharIndices = args[1].char_indices();
+        let mut idx: usize;
+        let len: usize = String::len(&args[1]);
+        loop {
+            idx = char_itr.next().unwrap().0;
+            let offset = char_itr.offset();
+            let loc = find_entry(0, DICT_LENGTH, &(args[1])[idx .. offset]);
+            println!("{}\t{}", DICT[loc][0], DICT[loc][1]);
+            if offset == len { break; }
         }
-    }
 
-    let output = Command::new("say")
+        let output = Command::new("say")
         .arg("-v")
         .arg("sin-ji")
         .arg(&args[1])
         .spawn();
-    assert!(output.is_ok());
+        assert!(output.is_ok());
+    }
 }
