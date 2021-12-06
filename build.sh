563e83efd3ade5bffdf39e8a368d2265cf3edec4
chungmcl <chungmcl@hotmail.com>
2021-12-06 15:25:36 -0800

    Remove C version. Made Rust version not depend on experimental lib stuff

--- build.sh
+++ build.sh
@@ -1,7 +1,2 @@
-if [ $1 = "rust" ]; then
-    python3 generate_header.py rust && rustc -C opt-level=3 rust/jyutping.rs 
-fi
-if [ $1 = "c" ]; then
-    python3 generate_header.py c && gcc -o jyutping c/jyutping.c
-fi
+python3 generate_header.py rust && rustc -C opt-level=3 rust/jyutping.rs
 exit 1
--- rust/jyutping.rs
+++ rust/jyutping.rs
@@ -1,7 +1,5 @@
-#![feature(char_indices_offset)]
 use std::env;
 use std::process::Command;
-use std::str::CharIndices;
 
 mod constants;
 use constants::DICT_LENGTH;
@@ -24,21 +22,43 @@ fn find_entry(l: usize, r: usize, x: &str) -> usize {
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() > 1 {
-        let mut char_itr: CharIndices = args[1].char_indices();
-        let mut curr: (usize, char);
-        let len: usize = String::len(&args[1]);
-        loop {
-            curr = char_itr.next().unwrap();
-            let offset = char_itr.offset();
+        let char_indices: Vec<(usize, char)> = args[1].char_indices().collect();
+        for idx in 0 .. char_indices.len() - 1 {
+            let curr: (usize, char) = *char_indices.get(idx).unwrap();
+            let offset: usize = char_indices.get(idx + 1).unwrap().0;
             let loc = find_entry(0, DICT_LENGTH, &(args[1])[curr.0 .. offset]);
+
             if loc < DICT_LENGTH {
                 println!("{}\t{}", DICT[loc][0], DICT[loc][1]);
             }
             else {
                 println!("{}", curr.1);
             }
-            if offset == len { break; }
         }
+        let curr: (usize, char) = *char_indices.get(char_indices.len() - 1).unwrap();
+        let loc = find_entry(0, DICT_LENGTH, &(args[1])[curr.0 .. args[1].len()]);
+        if loc < DICT_LENGTH {
+            println!("{}\t{}", DICT[loc][0], DICT[loc][1]);
+        }
+        else {
+            println!("{}", curr.1);
+        }
+
+        // This code is much cleaner, but requires: #![feature(char_indices_offset)]
+        //let mut curr: (usize, char);
+        //let len: usize = String::len(&args[1]);
+        //loop {
+        //    curr = char_itr.next().unwrap();
+        //    let offset = char_itr.offset();
+        //    let loc = find_entry(0, DICT_LENGTH, &(args[1])[curr.0 .. offset]);
+        //    if loc < DICT_LENGTH {
+        //        println!("{}\t{}", DICT[loc][0], DICT[loc][1]);
+        //    }
+        //    else {
+        //        println!("{}", curr.1);
+        //    }
+        //    if offset == len { break; }
+        //}
 
         if env::consts::OS == "macos" {
             let result = Command::new("say")
