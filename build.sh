1a15a619ba72fd1eb71f7411b24ce1538861a65e
chungmcl <chungmcl@hotmail.com>
2022-04-24 15:57:44 -0700

    fixed build script

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-macosClear
-#pip3 install yitizi && pip3 install pinyin && python3 generate_header.py && rustc -C opt-level=3 zh.rs
-rustc -C opt-level=3 zh.rs
+clear
+# macosClear
+pip3 install yitizi && pip3 install pinyin && python3 generate_header.py && rustc -C opt-level=3 zh.rs
+# rustc -C opt-level=3 zh.rs
 exit 1
